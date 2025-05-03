c2ff8a6a0f00987f29fd826eb7d623fdf619877b
Adrian <adri.ctrl.master@gmail.com>
2025-05-03 09:42:58 -0500

    Agrega script diario automatizado

---
+++ daily_task.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from scrapers.devto_scraper import scrap_devto
+from telegram_bot import enviar_resumen_diario
+
+def run_scraper():
+    print("Recolectando noticias...")
+    hoy = datetime.today().strftime('%Y-%m-%d')
+    archivo_salida = f"data/noticias_{hoy}.json"
+    scrap_devto(filename=archivo_salida)
+    print(f"Noticias guardadas en: {archivo_salida}")
+
+def main():
+    run_scraper()
+    enviar_resumen_diario()
+
+if __name__ == "__main__":
+    main()
